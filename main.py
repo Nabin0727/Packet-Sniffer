2b0472c4b766d82fdc4ed7df1e2621005af5cb06
Nabin Lopchan <lopchannabeen138@gmail.com>
2023-04-16 12:40:12 +0545

    third change

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ import struct
 import textwrap
 
 def main():
-    conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+
+    print("Starting packet sniffer...")
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    #conn.bind(('ens33', 0))
 
     while True:
-        raw_data, addr = conn.recvfrom(65536)
-        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-        print("\nEthernet Frame:")
-        print("Destination: {}, Source {}, Protocol {}".format(dest_mac, src_mac, eth_proto))
-
-
+        try:
+            raw_data, addr = conn.recvfrom(65535)
+            print("Packet received:")
+            dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+            print("Destination: {}, Source: {}, Protocol: {}".format(dest_mac, src_mac, eth_proto))
+        except socket.timeout:
+            # Socket timeout - no packets received
+            print("No packets received within timeout period.")
+        except Exception as e:
+            # Other exception - print error message and continue
+            print("Error receiving packet:", e)
+            continue
 
 # Unpack ethernet frame
 def ethernet_frame(data):
-    dest_mac, src_mac, proto = struct.unpack('! 6s 6s N', data[:14])
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
 
 # Return properly formatted MAC adderss (i.e AA:BB:CC:DD:EE:FF)
@@ -24,14 +33,5 @@ def get_mac_addr(bytes_addr):
     mac_addr = ':'.join(bytes_str).upper()
     return mac_addr
 
-# Unpack IPV4 Packet
-def ipv4_packet(data):
-    version_header_length = data[0]
-    version = version_header_length >> 4
-    header_length = (version_header_length & 15) * 4
-    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
-    return version, header_length, ttl, proto, ipv4_add(src), ipv4_add(target), data(header_length:)
 
-# Returns properly fromatted Ipv4 address
-def ipv4_add(addr):
-    return '.'.join(map(str, addr))
+main()

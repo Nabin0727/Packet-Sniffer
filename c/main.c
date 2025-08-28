7069ebf25e9ac7a0e66044f9c40c85c19eb884cd
Nabin0727 <lopchannabeen138@gmail.com>
2025-08-28 14:01:16 -0400

    initial commit for pcap

--- c/main.c
+++ c/main.c
@@ -1,1 +1,30 @@
 #include<stdio.h>
+#include<pcap.h>
+
+int main()
+{
+	char *dev = "eth0"; // interface
+	
+	char errbuf[PACP_ERRBUF_SIZE];
+
+	pcap_t *handel;
+
+	struct pcap_pkthdr header;
+
+	const u_char *packet;
+
+	// open interface
+	handel = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	
+	if(handel == NULL)
+	{
+		fprintf(stderr, "Couldn't open devices %s: %s\n", dev, errbuf);
+		return 2;
+	}
+
+	printf("Capturing packet on %s........\n", dev);
+
+
+	pcap_close(handel);
+	return 0;
+}
